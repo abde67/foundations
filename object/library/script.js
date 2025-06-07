969e5df9ee944804e0f0044c8bded44d2a3f9625
abde7 <abebechehite@gmail.com>
2025-06-07 22:28:06 +0300

    libray

--- object/library/script.js
+++ object/library/script.js
@@ -1,12 +1,20 @@
-const showBtn = document.getElementById("show-dialog");
-const dialog = document.getElementById("dialog");
-const jsCloseBtn = dialog.querySelector("#js-close");
+const showButton=document.getElementById("showDialog");
+const favDialog=document.getElementById("favDialog");
+const outputBox=document.querySelector("output");
+const selectEl=document.querySelector("select");
+const confirmBtn=document.querySelector("#confirmBtn");
 
-showBtn.addEventListener("click", () => {
-  dialog.showModal();
+
+showButton.addEventListener("click",()=>{
+  favDialog.showModal();
 });
 
-jsCloseBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  dialog.close();
+favDialog.addEventListener("close", (e)=>{
+  outputBox.value=
+  favDialog.returnValue==="default"? "noreturn value":`returnvalue:${favDialog.returnValue}.`
+
+});
+confirmBtn.addEventListener("click",(event)=>{
+  event.preventDefault();
+  favDialog.close(selectEl.value);
 });
