b5827a74e7ade7f7ebd61c0cf34f43f6d6d473d9
abde7 <abebechehite@gmail.com>
2025-06-08 00:05:55 +0300

    some js

--- object/library/script.js
+++ object/library/script.js
@@ -1,20 +1,24 @@
-const showButton=document.getElementById("showDialog");
-const favDialog=document.getElementById("favDialog");
-const outputBox=document.querySelector("output");
-const selectEl=document.querySelector("select");
-const confirmBtn=document.querySelector("#confirmBtn");
+
+  const addBooks=document.getElementById('addBooks');
+  const showBooks=document.getElementById('showBooks');
+const myLibrary=[];
+
+function Book(title, author,page, isRead){
+  this.title=title;
+  this.author=author;
+  this.page=page;
+  this.isRead=isRead;
+}
+
+function addBookToLibrary(){
+  myLibrary.push(Book);
+}
 
 
-showButton.addEventListener("click",()=>{
-  favDialog.showModal();
-});
 
-favDialog.addEventListener("close", (e)=>{
-  outputBox.value=
-  favDialog.returnValue==="default"? "noreturn value":`returnvalue:${favDialog.returnValue}.`
 
-});
-confirmBtn.addEventListener("click",(event)=>{
-  event.preventDefault();
-  favDialog.close(selectEl.value);
-});
+
+const divBook=document.createElement("div");
+divBook.setAttribute('id','divBook');
+
+showBooks.append(divBook)
