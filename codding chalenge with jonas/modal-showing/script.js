abb487ddae16368090a7e3b0d77198fde2a016b6
abde7 <abebechehite@gmail.com>
2025-07-30 23:51:15 +0300

    exerscices

---
+++ codding chalenge with jonas/modal-showing/script.js
@@ -0,0 +1,23 @@
+const modal=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+ const btnlosemodal=document.querySelector('.close-modal');
+ const  btnopenmodal=document.querySelectorAll('.show-modal')
+
+ const closemodal=function(){
+   modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+ }
+ const showmodal=function(){
+modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+ }
+
+ for (let i = 0; i < btnopenmodal.length; i++) {
+  btnopenmodal[i].addEventListener('click', showmodal)
+  
+ }
+
+
+ btnlosemodal.addEventListener('click',closemodal);
+ overlay.addEventListener('click',closemodal)
