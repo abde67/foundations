b85ee030798a6a07deab03a35e2896a8f5079db3
abde7 <abebechehite@gmail.com>
2025-08-03 23:53:31 +0300

    eidited exersice

--- codding-chalenge-with-jonas/dom-manipulation/script.js
+++ codding-chalenge-with-jonas/dom-manipulation/script.js
@@ -3,3 +3,9 @@
 //hellow will be the output b/c it is the first true falue
 console.log(undefined||0||""||'hellow'||23)
 
+
+// and oprator
+// 0 will be excuted b/c it is a false vaue
+console.log(0&&'jonas')
+// null will be excuted b/c it is a false vaue
+console.log('hello'&&23&&null&&'jonaas')
