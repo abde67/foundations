1abab1f66e952f42713e1e5b82cc7b71aab4ae34
abde7 <abebechehite@gmail.com>
2025-08-10 22:10:10 +0300

    jonas exersice bankist dom app

--- codding-chalenge-with-jonas/dom-manipulation/bankist-dom.netlify.app/script.js
+++ codding-chalenge-with-jonas/dom-manipulation/bankist-dom.netlify.app/script.js
@@ -32,3 +32,13 @@ overlay.addEventListener("click", closeModal);
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
 });
+
+
+
+
+const btnScrollTo=document.querySelector('.btn--scroll-to');
+const section1=document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click',function(e){
+  section1.scrollIntoView({behavior:'smooth'});
+});
