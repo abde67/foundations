5198b179394621493996732bea8bdccaf52db736
abde7 <abebechehite@gmail.com>
2025-08-12 19:59:33 +0300

    jonas exersice

--- codding-chalenge-with-jonas/dom-manipulation/bankist-dom.netlify.app/script.js
+++ codding-chalenge-with-jonas/dom-manipulation/bankist-dom.netlify.app/script.js
@@ -1,48 +1,83 @@
-const btnsOpenModel = document.querySelectorAll(".btn--open-modal");
-const btnCloseModal = document.querySelector(".btn--close-modal");
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const nav = document.querySelector(".nav");
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+const nav = document.querySelector('.nav');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
 
-//////////////////////////////////////////////////////////////////
-// LEC 2) PROJECT: "Bankist" Website
-
-// [SHOW PROJECT]
-// This code comes from the modal window we already built earlier. Let's just do 2 quick modifications here!
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
-  // 1.
-  // Prevent link from jumping to top of page. We used this before, and it always works the same: it prevents whatever would happen normally. And normally, a link with a # jumps to the section which has an ID with the same name [show in HTML]
   e.preventDefault();
-
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
 };
 
-const closeModal = function (e) {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
-// 2.
-btnsOpenModel.forEach((btn) => btn.addEventListener("click", openModal));
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
-btnCloseModal.addEventListener("click", closeModal);
-overlay.addEventListener("click", closeModal);
-document.addEventListener("keydown", function (e) {
-  if (e.key === "Escape" && !modal.classList.contains("hidden")) closeModal();
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
 });
 
 
-
-
-const btnScrollTo=document.querySelector('.btn--scroll-to');
-const section1=document.querySelector('#section--1');
+//button scrollng
 
 btnScrollTo.addEventListener('click',function(e){
   section1.scrollIntoView({behavior:'smooth'});
 });
 
+// document.querySelectorAll('.nav_link').forEach(function(el){
+//   el.addEventListener('click',function(e){
+//     e.preventDefault();
+//     const id=this.getAttribute('href');
+//     console.log(id); 
+//     document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+    
+//   });
+// });
 
-//ahfgadfghaewryfgsgfjagfjgasbcxzatreqqqqua
-//srpoerwlfl;lald;ftpd;fdogplffdogpelgsdl
+document.querySelector('.nav__links').addEventListener('click', function(e){
+
+   e.preventDefault();
+  if(e.target.classList.contains('nav__link')){
+   
+    const id=e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'});
+    
+  }
+})
+
+// tabbed component
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
