f89ea1021126b94119dff30532ae8fa82c8ded17
abde7 <abebechehite@gmail.com>
2025-08-18 02:08:39 +0300

    oop concept in js with jonas

--- codding-chalenge-with-jonas/OOP_in-js/script.js
+++ codding-chalenge-with-jonas/OOP_in-js/script.js
@@ -58,3 +58,34 @@ class CarCl {
     this.speed = speed * 1.6;
   }
 }
+
+
+
+class EVCl extends CarCl {
+  #charge;
+
+  constructor(make, speed, charge) {
+    super(make, speed);
+    this.#charge = charge;
+  }
+
+  chargeBattery(chargeTo) {
+    this.#charge = chargeTo;
+    return this;
+  }
+
+  accelerate() {
+    this.speed += 20;
+    this.#charge--;
+    console.log(
+      `${this.make} is going at ${this.speed} km/h, with a charge of ${
+        this.#charge
+      }`
+    );
+    return this;
+  }
+}
+
+const rivian = new EVCl('Rivian', 120, 23);
+console.log(rivian);
+// console.log(rivian.#charge);
