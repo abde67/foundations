663a742db7ee1f02ec6e1a75bca3b4e23690b155
abde7 <abebechehite@gmail.com>
2025-10-15 10:01:58 +0300

    exercises

--- jonas0ultimat-react-course/pizza-menu/src/index.js
+++ jonas0ultimat-react-course/pizza-menu/src/index.js
@@ -67,12 +67,34 @@ function Menu() {
   return (
     <main className="menu">
       <h2>our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="pizza spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Focaccia"
+        ingredients="Bread with italian olive oil and rosemary"
+        photoName="pizzas/focaccia.jpg"
+        price={12}
+      />
     </main>
   );
 }
+function Pizza(props) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
+    </div>
+  );
+}
+
 function Footer() {
   const hour = new Date().getHours();
   const openHour = 12;
@@ -85,16 +107,6 @@ function Footer() {
     <footer className="footer">
       {new Date().toLocaleTimeString()}.we're currently open!/
     </footer>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
   );
 }
 
