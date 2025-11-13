4b30c61d64de1b1bc2bd8e968ff99be93cf55ae3
abde7 <abebechehite@gmail.com>
2025-11-13 20:37:09 +0300

    react quiz src folder duplicated to change it using context API

---
+++ jonas0ultimat-react-course/10-react-quiz/src-no-context/components/Progres.js
@@ -0,0 +1,15 @@
+function Progres({ index, numQuestions, points, maxPossiblePoints, answer }) {
+  return (
+    <div className="progress">
+      <progress max={numQuestions} value={index + Number(answer !== null)} />
+      <p>
+        Question <strong>{index + 1}</strong>/ {numQuestions}
+      </p>
+      <p>
+        <strong>{points}</strong>/{maxPossiblePoints} points
+      </p>
+    </div>
+  );
+}
+
+export default Progres;
