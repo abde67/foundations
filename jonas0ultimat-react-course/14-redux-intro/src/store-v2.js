4b13f99749a43f0e2835ae963893fb04db8e8a38
abde7 <abebechehite@gmail.com>
2025-11-24 23:55:57 +0300

    v2 store.jsx file

---
+++ jonas0ultimat-react-course/14-redux-intro/src/store-v2.js
@@ -0,0 +1,16 @@
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import accountReducer from "./features/accounts/accountSlice";
+import customerRducer from "./features/customers/customerSlice";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@reduxjs/toolkit/devtools-extension";
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerRducer,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
