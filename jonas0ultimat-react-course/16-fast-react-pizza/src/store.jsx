1bd91036b518db4da837c6cdaf9b424da398c2d6
abde7 <abebechehite@gmail.com>
2026-01-01 13:16:00 +0300

    stor jsx for all pizza storre

--- jonas0ultimat-react-course/16-fast-react-pizza/src/store.jsx
+++ jonas0ultimat-react-course/16-fast-react-pizza/src/store.jsx
@@ -1,9 +1,13 @@
 
 import useReducer from "./features/user/userSlice"
 import { configureStore } from "@reduxjs/toolkit"
+import cartSlice from './features/cart/cartSlice'
+import cartReducer from './features/cart/cartSlice'
+
 const store=configureStore({
   reducer :{
     user:useReducer,
+    cart:cartReducer,
 
   },
 });
