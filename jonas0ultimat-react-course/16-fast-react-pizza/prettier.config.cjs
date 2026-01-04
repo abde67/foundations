2d4eefc0e90f72aff681f169ac030e8d5e06da4e
abde7 <abebechehite@gmail.com>
2026-01-04 12:13:05 +0300

    prettier configuration

--- jonas0ultimat-react-course/16-fast-react-pizza/prettier.config.cjs
+++ jonas0ultimat-react-course/16-fast-react-pizza/prettier.config.cjs
@@ -1,5 +1,4 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 module.exports = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: [require('prettier-plugin-tailwindcss')],
   singleQuote: true,
 };
