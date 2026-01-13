dbabb184fb28ac30755747eef3e369b4cceb4ceb
abde7 <abebechehite@gmail.com>
2026-01-13 09:27:24 +0300

    eslintrc.cjs

--- jonas0ultimat-react-course/17-the-wild-oasis/.eslintrc.cjs
+++ jonas0ultimat-react-course/17-the-wild-oasis/.eslintrc.cjs
@@ -2,7 +2,6 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-<<<<<<< HEAD
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
@@ -22,21 +21,3 @@ module.exports = {
     ],
   },
 };
-=======
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
->>>>>>> 5479f4d4c1532b781d04bba6ffe382a9429f3775
