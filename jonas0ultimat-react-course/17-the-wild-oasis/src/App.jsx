eedf093b577441caf2f4552f9fa3a7951b0e2ca1
abde7 <abebechehite@gmail.com>
2026-01-13 09:30:41 +0300

    app is modified it uses reactQuery

--- jonas0ultimat-react-course/17-the-wild-oasis/src/App.jsx
+++ jonas0ultimat-react-course/17-the-wild-oasis/src/App.jsx
@@ -1,24 +1,45 @@
-import styled from "styled-components";
-import GlobalStyle from "./styles/GlobalStyles";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyeldApp = styled.main`
-  background-color: red;
-  padding: 20px;
-`;
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Account from "./pages/Account";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import GlobalStyles from "./styles/GlobalStyles";
+import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60,
+    }, // 1 minute
+  },
+});
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <StyeldApp>
-        <Row />
-        <Heading as="h2">Welcome to The Wild Oasis</Heading>
-        <Heading as="h1">Welcome to The Wild Oasis</Heading>
-        <Heading as="h3">Welcome to The Wild Oasis</Heading>
-      </StyeldApp>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
