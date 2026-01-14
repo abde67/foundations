83fc809468cb71a401f32c4a3e1efbd60622736b
abde7 <abebechehite@gmail.com>
2026-01-14 16:03:02 +0300

    supabase js is modified

--- jonas0ultimat-react-course/17-the-wild-oasis/src/services/supabase.js
+++ jonas0ultimat-react-course/17-the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://rpnabgvngwfxfgxjudkj.supabase.co";
+export const supabaseUrl = "https://rpnabgvngwfxfgxjudkj.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwbmFiZ3ZuZ3dmeGZneGp1ZGtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgyMDE0MjIsImV4cCI6MjA4Mzc3NzQyMn0.errbTW8Bz-oDgROFuoN9tkfc1t9llLmSABfuR4rLn_U";
 
