e224e6a50a94bf526d6c6a6b5c1851f21fdf227a
abde7 <abebechehite@gmail.com>
2026-01-14 15:49:49 +0300

    App is modified

--- jonas0ultimat-react-course/17-the-wild-oasis/src/App.jsx
+++ jonas0ultimat-react-course/17-the-wild-oasis/src/App.jsx
@@ -11,10 +11,12 @@ import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60,
+      // staleTime: 1000 * 60,
+      staleTime: 0,
     }, // 1 minute
   },
 });
@@ -39,6 +41,26 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
