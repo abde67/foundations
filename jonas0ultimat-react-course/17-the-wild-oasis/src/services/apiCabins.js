e13456c8175ee4305047f32ed80514abe8d141b0
abde7 <abebechehite@gmail.com>
2026-02-08 11:35:45 +0300

    cabinapi ia modified to add an edit cabin feature

--- jonas0ultimat-react-course/17-the-wild-oasis/src/services/apiCabins.js
+++ jonas0ultimat-react-course/17-the-wild-oasis/src/services/apiCabins.js
@@ -12,17 +12,30 @@ export async function getCabins() {
   }
   return data;
 }
-export async function createCabin(newCabin) {
+export async function createEditCabin(newCabin,id) {
+const hasImagePath=newCabin.image?.startsWith?.(supabaseUrl
+  
+)
+
+
   const imageFile = newCabin.image[0];
   const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll("/","");
 
-  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-image/${imageName}`;
-
+  const imagePath = hasImagePath? newCabin.image:`${supabaseUrl}/storage/v1/object/public/cabin-image/${imageName}`;
+   
+  let query=supabase.from("cabins")
+  
+ 
   //1.create cabin
-  const { data, error } = await supabase
-    .from("cabins")
+   if (!id)
+  query=query 
     .insert([{ ...newCabin, image: imagePath }])
-    .select()
+
+
+  //TO EDIT
+if (!id) query= query.update({...newCabin,image:imagePath}).eq("id",id)
+
+    const {data,error}=await query.select()
     .single();
 
   if (error) {
